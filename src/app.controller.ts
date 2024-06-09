aeec8f4b798765df9b52f90ed3df4faa00e44923
visitor17564 <lanlanloo17564@gmail.com>
2024-06-10 02:13:05 +0900

    🧐 기본 api 체크완료

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): Promise<QueryDatabaseResponse> {
+    return this.appService.getStudentById();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Client } from '@notionhq/client';
 
 @Injectable()
 export class AppService {
+  private notion: Client;
+
+  constructor(private configService: ConfigService) {
+    this.notion = new Client({
+      auth: this.configService.get('NOTION_API_KEY'),
+    });
+  }
   getHello(): string {
+    // console.log(this.configService.get('NOTION_API_KEY'));
     return 'Hello World!';
+  }
+
+  async getStudentById() {
+    const databaseId = this.configService.get('NOTION_PEOPLE_DATABASE_ID');
+    const response = await this.notion.databases.query({
+      database_id: databaseId,
+      filter: {
+        property: 'id',
+        unique_id: {
+          equals: 1,
+        },
+      },
+    });
+
+    return response;
   }
 }
