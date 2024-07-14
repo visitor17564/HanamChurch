838aa2841defad9d7b610b9c5e14e16d19491471
visitor17564 <lanlanloo17564@gmail.com>
2024-07-15 03:56:50 +0900

    🫠MSSQL 깔고 연결까지 해놓음

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Client } from '@notionhq/client';
-import { promises } from 'dns';
+import sql from 'mssql';
+import { config } from '../config';
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
@@ -9,11 +10,39 @@ export class BoardService {
   private studentsDatabaseId: string;
   private checkDatabaseId: string;
   private commentsDatabaseId: string;
+  private config = {};
+  private poolconnection = null;
+  private connected = false;
 
   constructor() {
     this.studentsDatabaseId = process.env.NOTION_STUDENTS_DATABASE_ID;
     this.checkDatabaseId = process.env.NOTION_CHECK_DATABASE_ID;
     this.commentsDatabaseId = process.env.NOTION_COMMENTS_DATABASE_ID;
+    this.config = config;
+  }
+
+  async connect() {
+    try {
+      console.log(`Database connecting...${this.connected}`);
+      if (this.connected === false) {
+        this.poolconnection = await new sql.ConnectionPool(this.config);
+        this.connected = true;
+        console.log('Database connection successful');
+      } else {
+        console.log('Database already connected');
+      }
+    } catch (error) {
+      console.error(`Error connecting to database: ${JSON.stringify(error)}`);
+    }
+  }
+
+  async disconnect() {
+    try {
+      this.poolconnection.close();
+      console.log('Database connection closed');
+    } catch (error) {
+      console.error(`Error closing database connection: ${error}`);
+    }
   }
 
   async getBoard(
